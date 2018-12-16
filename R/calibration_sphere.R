8f8609dc34e44a1d33374acfdb28151bcbb9a0ce
Brandyn Lucca <45861253+brandynlucca@users.noreply.github.com>
2018-12-16 10:00:09 -0500

    Add files via upload

--- R/calibration_sphere.R
+++ R/calibration_sphere.R
@@ -78,7 +78,7 @@ ya <- function(l,n){return((ja(l,n) * cos(l*pi) - ja(-l,n)) / sin(l*pi))}
 
 #Import scipy.special module from Python for Bessel functions (referenced from AMOS)
 #Spherical Bessel function of first kind
-jl <- function(l,n,sign){ifelse(sign==1,ja(l+0.5,n) * sqrt(pi/2/n),spclja(l-0.5,n) * sqrt(pi/2/n))}
+jl <- function(l,n,sign){ifelse(sign==1,ja(l+0.5,n) * sqrt(pi/2/n),ja(l-0.5,n) * sqrt(pi/2/n))}
 #Spherical Bessel function of second kind
 yl <- function(l,n){ya(l+0.5,n) * sqrt(pi/2/n)}
 
