00833c005551ad1438b99c35e2c7ff30c5286629
Brandyn Lucca <brandyn.lucca@gmail.com>
2020-03-31 21:41:15 -0400

    Data documentation

---
+++ R/data.R
@@ -0,0 +1,18 @@
+#' Sample krill shape taken from McGehee et al. (1998)
+#'
+#' A dataset containing a sample krill (Euphausia superba) body shape proposed
+#'  by McGehee et al. (1998).
+#'
+#' @format A FFS object comprising all information required for target strength modelling. The variables are as follows:
+#' \describe{
+#'   \item{rpos}{Position matrix ([x,y,z], m)[0.0410898,0.0000000,0.0000000] to [0.0000000,0.0000000,0.003815]}
+#'   \item{a}{Radius vector (m) (0.0000000 to 0.0005946)}
+#'   \item{g}{Density contrast (1.0357)}
+#'   \item{h}{Soundspeed contrast (1.0279)}
+#'   \item{theta}{Orientation (degrees) (pi/2, or ~1.570796)}
+#'   \item{shape}{Body curvature (straight)}
+#'   \item{pc}{Radius of curvature (0)}
+#'   \item{L}{Length (m) (0.0410898)}
+#'   \item{ncyl}{Number of cylinders (15)}
+#' }
+"mcgehee"
