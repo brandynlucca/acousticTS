d702461f670f60bc0c72f41a5022b391dafd7fa4
Brandyn Lucca <45861253+brandynlucca@users.noreply.github.com>
2021-02-03 22:03:40 -0500

    Large changes to models, organization, documentation, etc.
    Extra functionality and support. Additional scatterer-class objects.

--- R/SDWBA.R
+++ R/SDWBA.R
@@ -1,149 +1,88 @@
-#' Calculates the theoretical TS of a fluid-like scatterer at a given frequency using the distorted Born wave approximation (DWBA) model.
-#'
-#' @param shape Desired object/animal shape. Must be class "FLS".
-#' @param c Sound speed of surrounding medium (m/s). Default value is 1500 m/s.
-#' @param frequency Frequency (Hz).
-#' @param x,y,z The x-, y-, and z-axis coordinates that make up the position matrix, \eqn{r_0}.
-#' @param a Radius vector of an animal (m).
-#' @param h Sound speed contrast.
-#' @param g Density contrast.
-#' @param pc Radius of curvature. Default is 3.3.
-#' @param curve A boolean value that dictates whether an animal is curved or not.
-#' @param phase Phase deviation (\eqn{\phi}), or phase variability. Accounts for complexities in animal shape and stochasticity of noise in scattering field.
-#' Default value is 0.0.
-#' @param theta Orientation of the target relative to the transmit source (\eqn{\theta}). Broadside incidence is considered 90 degrees, or pi/2.
-#' Default value is pi/2; input should be in radians.
-#' @param ncyl Number of segments comprising the scatterer shape.
-#' @usage
-#' SDWBA(shape, c, frequency, phase)
-#'
-#' SDWBA(shape=NULL, c, frequency, x, y, z, a, h, g, pc, curve, phase, theta, ncyl)
-#' @details
-#' Calculates the theoretical TS of a fluid-filled scatterer at a given frequency using the distorted Born wave approximation (DWBA) model.
-#' @return
-#' Target strength (TS, dB re: 1 m^2)
-#' @references
-#' Stanton, T.K., Chu, D., and Wiebe, P.H. 1998. Sound scattering by several zooplankton groups. II. Scattering models. Journal of the Acoustical Society of America, 103(1), 236-253.
-#'
-#' Hankin, R.K.S. 2006. Introducing elliptic, an R package for elliptic and modular functions. Journal of Statistical Software, 15(7).
-#' @export
-#' @import elliptic
-#' @import foreach
-#' @import doParallel
-#' @import parallel
-
-SDWBA <- function(shape=NULL, x=shape@rpos[1,], y=shape@rpos[2,], z=shape@rpos[3,],
-                  c=1500, frequency, phase=0.0, a=shape@a, h=shape@h, g=shape@g,
-                  curve=ifelse(is.null(shape),F,shape@curve),
-                  pc=ifelse(is.null(shape),
-                            ifelse(curve==T, 3.0, NA),
-                            ifelse(curve==T,
-                                   ifelse(shape@pc == 0, 3.0, shape@pc),
-                                   NA)),
-                  theta=ifelse(is.null(shape),pi/2,shape@theta),
-                  ncyl=ifelse(is.null(shape),length(x),shape@ncyl),
-                  L=shape@L,
-                  progress=TRUE){
-
-  rpos <- as.matrix(rbind(x,y,z))
-  kt <- cbind(cos(theta),rep(0,length(theta)),sin(theta))
-  k1 <- kcalc(frequency,c)*kt; k2 <- vecnorm(k1) / h
-  fbs <- 0 + 0i
-
-  for(j in 1:(ncyl-1)){
-    r1 <- rpos[,j]; r2 <- rpos[,j+1]
-    a1 <- a[j]; a2 <- a[j+1]
-    alpha <- acos((k1 %*% (r2-r1)) / (vecnorm(k1)*vecnorm(r2-r1)))
-    beta <- abs(alpha - pi/2)
-    # beta <- abs(acos(k1 %*% (r2 - r1)/(vecnorm(k1) * vecnorm(r2 - r1))) - pi/2)
-
-    SDWBAint <- function(s){
-      requireNamespace("elliptic", quietly = T)
-      rint <- s * (r2-r1)+r1
-      aint <- s * (a2-a1)+a1
-      gamma <- 1/(g*h^2)+1/g-2
-
-      if(abs(abs(beta) - pi/2) < 1e-10){
-        bessel <- k2*aint
-      }else{
-        bessel <- ja(1,2*k2*aint*cos(beta))/cos(beta)
-      }
-
-      if(curve == F){
-        ts <- vecnorm(k1)/4*gamma*aint*exp(2i*k1%*%rint/h)*bessel*vecnorm(r2-r1)
-        return(ts)
-      }else{
-        pct <- pc*L
-        # pct <- pc
-        ts <- vecnorm(k1)*pct/4*gamma*aint*exp(2i*k2*pct)*exp(-2i*k2*pct*cos(beta))*bessel*(vecnorm(r2-r1)/pct)
-        return(ts)
-      }
-    }
-
-    SDWBAint <- Vectorize(SDWBAint)
-    integral <- myintegrate(SDWBAint,0,1)
-    fbs <- fbs + integral * exp(1i * rnorm(1,0,phase))
-  }
-  return(20*log10(abs(fbs)))
-}
-
 #' Wrapper function that can simulate over distributions of values
 #'
 #' @param shape Desired object/animal shape. Must be class "FLS".
 #' @param c Sound speed of surrounding medium (m/s). Default value is 1500 m/s.
 #' @param frequency Frequency (Hz).
-#' @param x,y,z The x-, y-, and z-axis coordinates that make up the position matrix, \eqn{r_0}.
+#' @param x,y,z The x-, y-, and z-axis coordinates that make up the position 
+#' matrix, \eqn{r_0}.
 #' @param a Radius vector of an animal (m).
 #' @param h Sound speed contrast.
 #' @param g Density contrast.
 #' @param pc Radius of curvature. Default is 3.3.
-#' @param curve A boolean value that dictates whether an animal is curved or not.
-#' @param phase Phase deviation (\eqn{\phi}), or phase variability. Accounts for complexities in animal shape and stochasticity of noise in scattering field.
+#' @param curve A boolean value that dictates whether an animal is curved or 
+#' not.
+#' @param phase Phase deviation (\eqn{\phi}), or phase variability. Accounts 
+#' for complexities in animal shape and stochasticity of noise in scattering 
+#' field.
 #' Default value is 0.0.
-#' @param theta Orientation of the target relative to the transmit source (\eqn{\theta}). Broadside incidence is considered 0 degrees, or 0 radians. Animals are
-#' either tilted anterior-end down, which corresponds to (\eqn{-\pi/2}) and (\eqn{pi/2}) respectively. The model will automatically convert these orientations
-#' to the orientation definition used by McGehee et al. (1998) whereby orientation is based on the angle of the incident soundwave such that
-#' broadside incidence is \eqn{\pi/2} (dorsal-up) and \eqn{3\pi/2} (dorsal-down), or when the transmitted soundwave is completely perpendicular to the target.
+#' @param theta Orientation of the target relative to the transmit source 
+#' (\eqn{\theta}). Broadside incidence is considered 0 degrees, or 0 radians. 
+#' Animals are
+#' either tilted anterior-end down, which corresponds to (\eqn{-\pi/2}) 
+#' and (\eqn{pi/2}) respectively. The model will automatically convert these 
+#' orientations
+#' to the orientation definition used by McGehee et al. (1998) whereby 
+#' orientation is based on the angle of the incident soundwave such that
+#' broadside incidence is \eqn{\pi/2} (dorsal-up) and \eqn{3\pi/2} 
+#' (dorsal-down), or when the transmitted soundwave is completely perpendicular 
+#' to the target.
 #' Default value is pi/2; input should be in radians.
 #' @param length Option to change the length of the scatterer shape.
 #' @param nrep Number of repeated iterations to run the model.
-#' @param scale Log or linear domain. Log is the default, and outputs TS (dB re: 1 m^2). Linear represent \eqn{\sigma_bs} (m^2), which is the
-#' backscattering cross-section. Values must be in the linear domain as \eqn{\sigma_bs} if any averaging is going to be conducted on the output of
+#' @param scale Log or linear domain. Log is the default, and outputs TS 
+#' (dB re: 1 m^2). Linear represent \eqn{\sigma_bs} (m^2), which is the
+#' backscattering cross-section. Values must be in the linear domain as 
+#' \eqn{\sigma_bs} if any averaging is going to be conducted on the output of
 #' this model. Inputs allowed are "linear", "log", or "both".
-#' @param aggregate Options to aggregate dataframe output into a series of summary statistics. Options include "mean", "median", "minimum", and "maximum".
+#' @param aggregate Options to aggregate dataframe output into a series of 
+#' summary statistics. Options include "mean", "median", "minimum", and "maximum".
 #' @param permute Calculates for every permutation of variables.
-#' @param parallel Boolean value that sets whether multicore CPU parallelization will be used to speed up calculations.
-#' @param n.cores Number of CPU cores that will be dedicated to parallelizing model calculations.
+#' @param parallel Boolean value that sets whether multicore CPU parallelization 
+#' will be used to speed up calculations.
+#' @param n.cores Number of CPU cores that will be dedicated to parallelizing 
+#' model calculations.
 #' @usage
 #' SDWBA.sim(shape, c=1500, frequency, phase=0.0, curve=F, nrep=NULL, permute=F,
 #' aggregate=NULL, parallel=F, n.cores=NULL)
 #'
-#' SDWBA.sim(shape, c=1500, frequency, phase=0.0, h, g, curve=F, pc, theta, length,
+#' SDWBA.sim(shape, c=1500, frequency, phase=0.0, h, g, curve=F, pc, theta, 
+#' length,
 #' nrep=NULL, permute=F, aggregate=NULL, parallel=F, n.cores=NULL)
 #'
-#' SDWBA.sim(shape, c=1500, frequency, phase=0.0, x, y, z, a, h, g, curve=F, pc, theta, length,
+#' SDWBA.sim(shape, c=1500, frequency, phase=0.0, x, y, z, a, h, g, curve=F, 
+#' pc, theta, length,
 #' nrep=NULL, permute=F, aggregate=NULL, parallel=F, n.cores=NULL)
 #' @details
-#' Calculates the theoretical TS of a fluid-filled scatterer at a given frequency using the distorted Born wave approximation (DWBA) model.
+#' Calculates the theoretical TS of a fluid-filled scatterer at a given 
+#' frequency using the distorted Born wave approximation (DWBA) model.
 #' @return
 #' Target strength (TS, dB re: 1 m^2)
 #' @references
-#' Stanton, T.K., Chu, D., and Wiebe, P.H. 1998. Sound scattering by several zooplankton groups. II. Scattering models. Journal of the Acoustical Society of America, 103(1), 236-253.
+#' Stanton, T.K., Chu, D., and Wiebe, P.H. 1998. Sound scattering by several 
+#' zooplankton groups. II. Scattering models. Journal of the Acoustical Society 
+#' of America, 103(1), 236-253.
 #'
-#' Hankin, R.K.S. 2006. Introducing elliptic, an R package for elliptic and modular functions. Journal of Statistical Software, 15(7).
+#' Hankin, R.K.S. 2006. Introducing elliptic, an R package for elliptic and 
+#' modular functions. Journal of Statistical Software, 15(7).
 #' @export
-#' @import elliptic
 #' @import foreach
 #' @import doSNOW
+#' @import snow
+#' @importFrom parallel detectCores
+#' @import stats
+#' @import methods
 
-SDWBA.sim <- function(shape=shape, x=shape@rpos[1,], y=shape@rpos[2,], z=shape@rpos[3,],
-                      c=1500, frequency, phase=0.0, a=shape@a, h=shape@h, g=shape@g,
+SDWBA.sim <- function(shape=shape, 
+                      x=shape@rpos[1,], y=shape@rpos[2,], z=shape@rpos[3,],
+                      c=1500, frequency, phase=0.0, a=shape@a, 
+                      h=shape@h, g=shape@g,
                       curve=shape@curve,
                       pc=shape@pc,
                       theta=shape@theta,
                       length=shape@L,
                       scale="log",
-                      nrep=NULL, permute=F, aggregate=NULL, parallel=F, n.cores=NULL,
+                      nrep=NULL, permute=F, aggregate=NULL, parallel=F, 
+                      n.cores=NULL,
                       progress=T){
 
   if(!is.null(nrep)){
@@ -160,28 +99,33 @@ SDWBA.sim <- function(shape=shape, x=shape@rpos[1,], y=shape@rpos[2,], z=shape@r
     baselen <- data.frame(param=c("repseq", "c", "frequency"),
                           len=c(length(repseq), length(c), length(frequency)))
     baseprod <- prod(baselen$len)
-    paramlen <- data.frame(param=c("h","g","curve","pc","theta","length","phase"),
+    paramlen <- data.frame(param=c("h","g","curve","pc",
+                                   "theta","length","phase"),
                            len=c(length(h),length(g),length(curve),length(pc),
                                  length(theta),length(length),length(phase)))
     simdf <- data.frame(iterator=seq_len(baseprod), c=c, frequency=frequency)
     simdf <- cbind(simdf,
                    data.frame(g=rep(g, baseprod), h=rep(h, baseprod),
-                              theta=rep(theta, baseprod), pc=rep(pc, baseprod),
-                              curve=rep(curve, baseprod), length=rep(length, baseprod),
+                              theta=rep(theta, baseprod), 
+                              pc=rep(pc, baseprod),
+                              curve=rep(curve, baseprod), 
+                              length=rep(length, baseprod),
                               phase=rep(phase, baseprod),
                               TS=NA))
     simdf <- simdf[,-1]
   }
 
   if(progress == T){
-    bar <- txtProgressBar(min=0, max=nrow(simdf), style=3, title="Calculating TS values...")
+    bar <- txtProgressBar(min=0, max=nrow(simdf), style=3, 
+                          title="Calculating TS values...")
     progbar <- function(n) setTxtProgressBar(bar, n)
   }
 
 
   if(parallel==F){
     for(i in 1:nrow(simdf)){
-      target_sim <- Shapely(shape,curve=simdf$curve[i],pc=simdf$pc[i],theta=simdf$theta[i],length=simdf$length[i])
+      target_sim <- Shapely(shape,curve=simdf$curve[i],pc=simdf$pc[i],
+                            theta=simdf$theta[i],length=simdf$length[i])
       simdf$TS[i] <- SDWBA(target_sim,c=simdf$c[i],frequency=simdf$frequency[i],
                            phase=simdf$phase[i],g=simdf$g[i],h=simdf$h[i],
                            curve=simdf$curve[i],pc=simdf$pc[i])
@@ -191,7 +135,6 @@ SDWBA.sim <- function(shape=shape, x=shape@rpos[1,], y=shape@rpos[2,], z=shape@r
       }
     }
   }else if(parallel==T){
-    requireNamespace("parallel", quietly=T)
     requireNamespace("doSNOW", quietly=T)
     requireNamespace("snow", quietly=T)
 
@@ -232,7 +175,8 @@ SDWBA.sim <- function(shape=shape, x=shape@rpos[1,], y=shape@rpos[2,], z=shape@r
   if(!is.null(aggregate)){
     dum <- data.frame(Stat=NA,TS=NA)
     if("mean" %in% aggregate){
-      dum <- rbind(dum,data.frame(Stat="Mean", TS=10*log10(mean(10^(simdf$TS/10)))))
+      dum <- rbind(dum,data.frame(Stat="Mean", 
+                                  TS=10*log10(mean(10^(simdf$TS/10)))))
     }
     if("median" %in% aggregate){
       dum <- rbind(dum,data.frame(Stat="Median", TS=median(simdf$TS)))
--- R/classes.R
+++ R/classes.R
@@ -1,118 +1,152 @@
+#' Scattering-class objects.
+#' 
+#' @description
+#' The group of S4 class objects that represents different scatterer-types, such
+#' as fluid-like (FLS) and gas-bearing (GAS) animals. In general, each of these 
+#' classes contain relevant animal metadata for parameterizing models, such 
+#' as orientation (\eqn{\theta}), material properties (\eqn{\rho} and g, 
+#' c and h), and body morphology (e.g., \eqn{r_0}).
+#' 
+#' @details
+#' There are currently several scattering-classes available for modeling using
+#' the \eqn{acousticTS} package:
+#' 
+#' \itemize{
+#'  \item \strong{FLS}: \code{\link[acousticTS; classes; FLS]{fluid-like 
+#'  scatterers (FLS)}} defined 
+#'  by the FLS-class. These generally describe scatterers (or body structures) 
+#'  that are fluid-like, meaning that the acoustic material properties (i.e., 
+#'  sound speed and density) are similar to that of the surrounding medium. 
+#'  Consequently, things like thin flesh and crustaceans are typically modeled 
+#'  as FLS objects. Current models available to FLS objects includes the 
+#'  \link[acousticTS; models]{SDWBA}, \link[acousticTS; models]{KRM}, and 
+#'  \link[acousticTS; models]{DCM}.
+#'  \item \strong{SBF}: `swimbladdered fish (SBF)` defined by the SBF-class. 
+#'  \item \strong{CAL}: `solid and calibration spheres (CAL)` defined by the 
+#'  CAL-class. 
+#'  \item \strong{GAS}: `gas-bearing scatterers (GAS)` defined by the GAS-class.
+#'  \item \strong{ESS}: `elastic shelled scatterers (ESS)` defined by the 
+#'  ESS-class.
+#' }
+#' 
+#' @export
+scattering <- setClass("scattering", representation("VIRTUAL"))
+
 #' Fluid-like scatterer (FLS) object/class.
 #'
 #' @description
-#' A S4 class that provides slots to contain relevant animal metadata for parameterizing models for fluid-like scatterers (FLS) partitioned
-#' into discretized cylinders. This, specifically, includes a position matrix, radius, material properties (g, h), orientation,
-#' animal shape, and body curvature. This class is used within the DWBA and DFCM model functions. In the future, this will also allow for
-#' converting one class of scatterer into another for seemless usage for model comparisons.
+#' A S4 class that provides slots to contain relevant animal metadata for 
+#' parameterizing models for fluid-like scatterers (FLS) partitioned into 
+#' discretized cylinders. This, specifically, includes a position matrix, 
+#' radius, material properties (g, h), orientation, animal shape, and body 
+#' curvature. This class is used within the DWBA and DFCM model functions. In 
+#' the future, this will also allow for converting one class of scatterer into 
+#' another for seemless usage for model comparisons.
 #'
-#' @param rpos A 3xN matrix, where N is the number of columns, or segments, in our scatterer object. The rows represent the x-, y-, and z-axis
+#' @param rpos A 3xN matrix, where N is the number of columns, or segments, in 
+#' our scatterer object. The rows represent the x-, y-, and z-axis
 #' shape values in that order. Units in meters.
 #' @param a An N-length radius vector. Units in meters.
 #' @param g Density contrast. Units are dimensionless.
 #' @param h Sound speed contrast. Units are dimensionless.
-#' @param theta Animal orientation, where \eqn{\pi/2} is broadside incidence. Units in radians.
-#' @param curve Body curvature/flexure. Boolean (T/F) which flags the appropriate model formulation.
+#' @param theta Animal orientation, where \eqn{\pi/2} is broadside incidence. 
+#' Units in radians.
+#' @param curve Body curvature/flexure. Boolean (T/F) which flags the a
+#' ppropriate model formulation.
 #' @param pc Radius of curvature ratio. Units are dimensionless.
 #' @param L Length of target. Units in meters.
-#' @param ncyl Number of segments/cylinders comprising the shape. Units should be an integer value.
+#' @param ncyl Number of segments/cylinders comprising the shape. Units should 
+#' be an integer value.
 #'
 #' @details
-#' FLS objects can be created using \link[acousticTS]{FLSwrite} and \link[acousticTS]{FLSgenerate}. These objects can also be read using
-#' built-in datasets (e.g., \link[acousticTS]{mcgehee}) or using \link[acousticTS]{FLSread}. The \link[acousticTS]{SDWBA}, \link[acousticTS]{SDWBA.sim},
-#' \link[acousticTS]{DFCM}, and \link[acousticTS]{Shapely} functions all use FLS-objects for their respective inputs.
+#' FLS objects can be created using 
+#' \link[acousticTS; class_utilities]{FLSwrite} and 
+#' \link[acousticTS; class_utilities]{FLSgenerate}. These objects can also be 
+#' read using
+#' built-in datasets (e.g., \link[acousticTS]{mcgehee}) or using 
+#' \link[acousticTS; class_utilities]{FLSread}. The 
+#' \link[acousticTS; models]{SDWBA}, 
+#' \link[acousticTS; SDWBA]{SDWBA.sim}, \link[acousticTS; models]{DCM}, and 
+#' \link[acousticTS; utilities]{Shapely} functions all use FLS-objects for their 
+#' respective 
+#' inputs.
 #'
 #' @export
+#' @import methods
 
-FLS <- setClass("FLS",slots=c(rpos="matrix",a="numeric",g="numeric",h="numeric",theta="numeric",curve="logical",pc="numeric",L="numeric",ncyl="numeric"))
-
-#' Calls in a *.csv file as a FLS object
-#'
-#' @param file A *.csv file formatted with the following columns: x, y, z, a, g, h, theta [optional], pc [optional]
-#' @usage
-#' FLSread(file)
-#' @return
-#' Calls in an FLS-class object from a *.csv file
-#' @export
-
-FLSread <- function(file){
-  animal <- read.csv(file, header=T) #Call in *.csv file; assumes headers are present
-  return(new("FLS", rpos=as.matrix(rbind(animal$x,animal$y,animal$z)),a=animal$a,
-             g=animal$g[1],h=animal$h[1],
-             theta=ifelse(length(animal$theta) > 0, animal$theta, pi/2),
-             curve=F,pc=0.0,L=max(animal$x),ncyl=length(animal$x)))}
-
-#' Manually generate a FLS object.
-#'
-#' @param x Vector containing x-axis body (m) shape data.
-#' @param y Vector containing y-axis body (m) shape data.
-#' @param z Vector containing z-axis body (m) shape data.
-#' @param a Vector containing radii (m).
-#' @param g Density contrast.
-#' @param h Soundspeed contrast
-#' @param theta Orientation of the target relative to the transmit source (\eqn{\theta}). Broadside incidence is considered 90 degrees, or pi/2.
-#' Default value is pi/2; input should be in radians.
-#' @usage
-#' FLSgenerate(x,y,z,a,g,h,theta)
-#' @examples
-#' #Manually parameterize shape
-#' x <- seq(1,10,1)*1e-3; y <- rep(0,10); z <- c(seq(1,5,1),rev(seq(1,5,1)))*1e-4
-#' a <- z/2
-#' g <- 1.036
-#' h <- 1.0279
-#' new_target <- FLSgenerate(x=x,y=y,z=z,a=a,g=g,h=h)
-#' #Let's model where sound speed (c) is 1500 m/s, frequency is 120 kHz, with no phase deviation
-#' c <- 1500
-#' freq <- 120e3
-#' SDWBA(shape=new_target, c=c, frequency=freq)
-#' [1] -114.0107
-#' @return
-#' Calls in an FLS-class object from a *.csv file
-#' @export
-#Manual shape creation
-##Inputs required:
-##x,y,z for position matrix, a for radius, g and h for material properties, theta for tilt angle (default = 0.0 deg)
-FLSgenerate <- function(x,y,z,a,g,h,theta=pi/2,curve=F,pc=0.0){
-  return(new("FLS", rpos=as.matrix(rbind(x,y,z)),a=a,g=g,h=h,theta=theta,curve=curve,pc=pc,L=max(x),ncyl=length(x)))}
-
-#' @export
-FLSwrite <- function(shape, filename=paste(getwd(),"/target_shape_",Sys.Date(),".csv",sep="")){
-  object <- data.frame(x=shape@x,y=shape@y,z=shape@z,a=shape@a,h=rep(shape@h,shape@ncyl),g=rep(shape@g,shape@ncyl))
-  write.csv(shape, file=filename)
-}
+FLS <- setClass("FLS",slots=c(rpos="matrix",a="numeric",
+                              g="numeric",h="numeric",
+                              theta="numeric",curve="logical",pc="numeric",
+                              L="numeric",ncyl="numeric"),
+                contains="scattering")
 
 #' Swimbladdered fish (SBF) object/class.
 #'
 #' @description
-#' A S4 class that provides slots to contain relevant animal metadata for parameterizing models for swimbladdered fish/scatterers (SBF)
-#' partitioend into two sets of discretized cylinders: the body and swimbladder. Both shapes comprise a position matrix, material properties
-#' (sound speed, c, and density, rho), orientation (theta), the overall body length of the animal, and the number of cylinders. This class is
+#' A S4 class that provides slots to contain relevant animal metadata for 
+#' parameterizing models for swimbladdered fish/scatterers (SBF)
+#' partitioend into two sets of discretized cylinders: the body and 
+#' swimbladder. Both shapes comprise a position matrix, material properties
+#' (sound speed, c, and density, rho), orientation (theta), the overall body 
+#' length of the animal, and the number of cylinders. This class is
 #' used within the KRM and GBZ model functions.
 #' @export
-SBF <- setClass("SBF", slots=c(body="matrix",bladder="matrix",theta="numeric",pb="numeric",
-                               cb="numeric",psb="numeric",csb="numeric",L="numeric",
-                               ncylb="numeric",ncylsb="numeric"))
+SBF <- setClass("SBF", slots=c(body="matrix",bladder="matrix",theta="numeric",
+                               pb="numeric",
+                               cb="numeric",psb="numeric",csb="numeric",
+                               L="numeric",
+                               ncylb="numeric",ncylsb="numeric"),
+                contains="scattering")
 
-#' Calls in a *.csv file as a SBF
+#' Solid and calibration sphere (CAL) object/class.
 #'
-#' @param file A *.csv file formatted with the following columns: xb, wb, zbU, zbL, xsb, wsb, zsbU, zsbL, pb, cb, psb, csb, theta [optional].
-#' @usage
-#' SBFread(file)
-#' @return
-#' Calls in an SBF-class object from a *.csv file
+#' @description
+#' A S4 class that provides slots to contain relevant metadata for solid sphere
+#' objects belonging to the CAL-class scatterers.
+#' @param material Material-type for the soldi sphere. See 'Details' built-in
+#' material options. 
+#' @param a Spherical radius (m).
+#' @param c1 Longitudinal sound speed (m/s).
+#' @param c2 Transversal sound speed (m/s).
+#' @param rho1 Density (kg/m^3)
+#'
+#' @details
+#' There are several options for the \strong{material} argument:
+#' \tabular{rlllll}{
+#'  \strong{Material} \tab \strong{Argumment} \tab \strong{c1} \tab \strong{c2} 
+#'  \tab \strong{\eqn{\rho1}}\cr
+#'  \emph{Tungsten carbide} \tab "WC" \tab 6853 \tab 4171 \tab 8360\cr
+#'  \emph{Stainless steel} \tab "steel" \tab 5980 \tab 3297 \tab 7970\cr
+#'  \emph{Brass} \tab "brass" \tab 4372 \tab 2100 \tab 8360\cr
+#'  \emph{Copper} \tab "Cu" \tab 4760 \tab 2288.5 \tab 8947\cr
+#'  \emph{Aluminum} \tab "Al" \tab 6260 \tab 3080 \tab 2700\cr
+#' }
+#' 
 #' @export
-SBFread <- function(file){
-  animal <- read.csv(file, header=T)
-  return(new("SBF",
-             body=as.matrix(rbind(animal$xb, animal$wb, animal$zbU, animal$zbL)),
-             bladder=as.matrix(rbind(animal$xsb[!is.na(animal$xsb)], animal$wsb[!is.na(animal$xsb)],
-                                     animal$zsbU[!is.na(animal$xsb)], animal$zsbL[!is.na(animal$xsb)])),
-             pb=animal$pb[1], cb=animal$cb[1], psb=animal$psb[1], csb=animal$csb[1],
-             theta=ifelse(length(animal$theta) > 0, animal$theta, pi/2), L=max(animal$xb),
-             ncylb=length(animal$xb[!is.na(animal$xb)]),ncylsb=length(animal$xsb[!is.na(animal$xsb)])))}
+CAL <- setClass("CAL", slots=c(material="character", 
+                               a="numeric",
+                               c1="numeric", c2="numeric", rho1="numeric"),
+                contains="scattering")
 
-#' Generate SBF shape
+#' Elastic shelled scatterer (ESS) object/class.
+#'
+#' @description
+#' A S4 class that provides slots to contain relevant metadata for elastic 
+#' shelled scatterers/objects belonging to the ESS-class scatterers.
+#' 
+#' @param a Radius (m).
+#' @param g Density contrast. 
+#' @param h Sound speed contrast.
+#' @param theta Orientation (\eqn{\theta}, radians)
+#' 
+#' @param L Optional. Length of the longitudinal axis (m) for non-spherical 
+#' shapes.
+#' @param spherical Optional. Boolean for specifying whether scatterer is 
+#' spherical or oblong. 
+#' 
 #' @export
-SBFgenerate <- function(xb,wb,zbU,zbL,xsb,wsb,zsbU,zsbL,pb,cb,psb,csb,theta=pi/2){
-  return(new("SBF", body=as.matrix(rbind(xb,wb,zbU,zbL)), bladder=as.matrix(rbind(xsb,wsb,zsbU,zsbL)),
-             pb=pb, cb=cb, psb=psb, csb=csb, theta=theta, L=max(xb), ncylb=length(xb), ncylsb=length(xsb)))}
+ESS <- setClass("ESS", slots=c(L="numeric",
+                               a="numeric", 
+                               g="numeric", h="numeric", theta="numeric",
+                               spherical="logical"), 
+                contains="scattering")
