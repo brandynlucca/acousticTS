e1d0a4679fe578f008484df4e7aa58e1e3dec2bc
brandynlucca <brandyn.lucca@gmail.com>
2025-11-29 22:46:34 -0800

    Re-add missing param.f90 file

---
+++ src/param.f90
@@ -0,0 +1,8 @@
+module param
+    integer, parameter :: vb_knd = selected_real_kind(15)
+    integer, parameter :: vb_knd1 = selected_real_kind(15)
+    logical, parameter :: debug = .true.
+    logical, parameter :: warn = .true.
+    logical, parameter :: output = .false.
+    logical, parameter :: suffix = .false.
+end module param
