f1793a143cf81aeb53a09a8206b59cba568c7c3b
brandynlucca <brandyn.lucca@gmail.com>
2026-04-26 23:37:04 -0700

    Reduce `testthat.R`

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,22 +1,4 @@
 library(testthat)
 library(acousticTS)
 
-local({
-  close_test_devices <- function() {
-    while (grDevices::dev.cur() > 1L) {
-      try(grDevices::dev.off(), silent = TRUE)
-    }
-  }
-
-  on.exit(
-    {
-      close_test_devices()
-      invisible(gc())
-    },
-    add = TRUE
-  )
-
-  test_check("acousticTS", reporter = testthat::SummaryReporter$new())
-  close_test_devices()
-  invisible(gc())
-})
+test_check("acousticTS", reporter = testthat::SummaryReporter$new())
